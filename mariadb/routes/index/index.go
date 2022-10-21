530c7ec767f0703dd2b3db2a3d8e452add6c3153
Anthony Salemo <ajssalemo@gmail.com>
2022-10-21 18:45:28 -0400

    correcting index route names

--- mariadb/routes/index/index.go
+++ mariadb/routes/index/index.go
@@ -11,7 +11,7 @@ func IndexRoute(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	resp := make(map[string]string)
-	resp["message"] = "Hello, from go-database-samples - MySQL"
+	resp["message"] = "Hello, from go-database-samples - MariaDB"
 	jsonResp, err := json.Marshal(resp)
 	if err != nil {
 		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
